aff4f3a90973b07d4a92b596e8ff59c5e85d96f1
sidhantrajput1 <sidhant808310@gmail.com>
2025-08-18 05:40:00 +0530

    Project Setup and Add Shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="">
-      Home
+      <Button variant={"outline"} className="m-4">ShadCn Button</Button>
     </div>
   );
 }
